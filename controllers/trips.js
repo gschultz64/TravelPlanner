524d649a61c866debe9a7d409e3cabb56b3dbd6d
Genevieve Schultz <genschultz@gmail.com>
2018-07-01 22:08:56 -0700

    locations can be added successfully from map search results to a specific trip, and ids are stored in join table

--- controllers/trips.js
+++ controllers/trips.js
@@ -23,14 +23,14 @@ router.get('/new', function (req, res) {
 });
 
 // GET - /trips/:id - display a specific trip and its locations
-router.get('/:id', function(req, res) {
+router.get('/:id', function (req, res) {
   db.trip.find({
-    where: {id: req.params.id},
+    where: { id: req.params.id },
     include: [db.location]
-  }).then(function(trip) {
+  }).then(function (trip) {
     if (!trip) throw Error();
-    res.render('trips/show', {trip: trip});
-  }).catch(function(error) {
+    res.render('trips/show', { trip: trip, locations: locations });
+  }).catch(function (error) {
     console.log(error);
     res.status(400).render('404');
   });
@@ -39,47 +39,58 @@ router.get('/:id', function(req, res) {
 // GET - /trips/:id/edit - display form to update a trip's details and/or add locations
 router.get('/:id/edit', function (req, res) {
   db.trip.find({
-    where: {id: req.params.id}
-  }).then(function(data) {
-  res.render('trips/edit', {trip: data});
+    where: { id: req.params.id }
+  }).then(function (data) {
+    res.render('trips/edit', { trip: data });
   });
 });
 
-//PUT - /trips/:id - update a trip (locations and/or trip details)
-router.put('/:id/', function(req,res) {
+//PUT - /trips/:id - update the details of a  trip (name, dates)
+router.put('/:id/', function (req, res) {
   db.trip.update({
     name: req.body.name,
     startDate: req.body.startDate,
     endDate: req.body.endDate
   }, {
-    where: {id: req.params.id}
-  }).then(function(data) {
-    db.location.findOrCreate({
-      where: { placeId: req.body.placeId }
-    }).spread(function (location, created) {
-      trip.addLocation(location).then(function (location) {
-        console.log(location + 'added to', trip);
-      });
+      where: { id: req.params.id }
+    }).then(function (data) {
+      res.sendStatus(200);
     });
-    res.sendStatus(200);
-  });
 });
 
 
 // POST - /trips - add a new trip
-router.post('/', function(req,res) {
-  // var id = req.params.id;
+router.post('/', function (req, res) {
   db.trip.findOrCreate({
     where: {
       name: req.body.name,
       startDate: req.body.startDate,
       endDate: req.body.endDate
     }
-  }).spread(function(trip, created) {
-    console.log(JSON.stringify(trip));
-    
-    res.redirect('/trips/' + trip.id );
+  }).spread(function (trip, created) {
+    res.redirect('/trips/' + trip.id);
   }).catch(function (error) {
+    console.log(error);
+    res.status(400).render('404');
+  });
+});
+
+// POST - /trips/:id - add locations to a trip
+router.post('/:id', function (req, res) {
+  db.trip.findById(req.params.id).then(function (trip) {
+    db.location.findOrCreate({
+      where: {
+        placeId: req.body.placeId,
+        name: req.body.placeName,
+        address: req.body.address
+      }
+    }).spread(function (location, created) {
+      trip.addLocation(location).then(function (location) {
+        console.log(location + 'added to', trip);
+        res.redirect('/trips/' + trip.id);
+      });
+    }); 
+  }).catch(function(error){
     console.log(error);
     res.status(400).render('404');
   });
@@ -87,10 +98,10 @@ router.post('/', function(req,res) {
 
 
 //DELETE - /trips/:id - delete a trip 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', function (req, res) {
   db.trip.destroy({
-    where: {id: req.params.id}
-  }).then(function(data) {
+    where: { id: req.params.id }
+  }).then(function (data) {
     console.log(data);
     res.sendStatus(200);
   });
--- public/js/script.js
+++ public/js/script.js
@@ -28,6 +28,7 @@ $(document).ready(function() {
       window.location = url
     });
   });
+  
 
   $('a.delete').on('click', function(e) {
     e.preventDefault();
