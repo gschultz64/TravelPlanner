4b2fb33accc0669b6097dbb5be4c454075ca305a
Genevieve Schultz <genschultz@gmail.com>
2018-07-02 01:20:20 -0700

    sequelize struggles

--- models/trip.js
+++ models/trip.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   trip.associate = function(models) {
     // associations can be defined here
-    models.trip.belongsTo(models.user);
+    models.trip.belongsToMany(models.user);
     models.trip.belongsToMany(models.location, { through: "locationsTrips" });
   };
   return trip;
